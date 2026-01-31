7f145abdd28d1e5b3189b3d01a0cba54dbf1c9a8
Bhanu Vedith Reddy Pinnapureddy <vedithreddy210@gmail.com>
2026-01-31 10:26:58 +0530

    Delete OneDrive/Desktop/Chat_bot/GAN directory

--- OneDrive/Desktop/Chat_bot/GAN/app/dependencies.py
+++
@@ -1,5 +0,0 @@
-from app.gan_engine import GANEngine
-gen_instance = GANEngine()
-def get_gan_engine():
-    gan_instance = GANEngine()
-    return gan_instance
--- OneDrive/Desktop/Chat_bot/GAN/app/schemas.py
+++
@@ -1,4 +0,0 @@
-from pydantic import BaseModel
-class GenerateRequest(BaseModel):
-    seed: int
-    truncation: float = 0.7
